8be234278f4bac2c060f4b60e6f22d7317c471b1
Jesse Ashmore <Jesse.ashmore97@gmail.com>
2018-02-20 02:26:06 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 function calculateAvg(){
     var table = document.getElementById("table-results");
-    for(var i; i<table.length; i++){
+    for (var i = 1, row; row = table.rows[i]; i++){
         var rowTotal = 0;
-        for(var j; j<table.length; j++){
-            if(table.rows[i].cells[j].className === 'table-result' && table.rows[i].cells[j].innerHTML !== '-'){
-                rowTotal +=
+        var entries = 0;
+        for (var j = 0, col; col = row.cells[j]; j++){
+            console.log(col.innerText);
+            if(j > 1 && col.innerText !== "-%" && col.innerText !== "-" && col.className !== "table-final-grade"){
+                rowTotal = rowTotal + parseFloat(col.innerText.slice(0, -1));
+                entries = entries + 1;
+            }
+            if(col.className === "table-final-grade"){
+                var mean = 0;
+                if(entries > 0){
+                    mean = Math.round(rowTotal/entries);
+                }
+                if(mean < 40){
+                    col.style.backgroundColor = "#990000";
+                    col.style.color = "white";
+                }else{
+                    col.style.backgroundColor = "#ffffff";
+                    col.style.color = "black";
+                }
+                col.innerText = mean.toString();
             }
         }
     }
+
 }
