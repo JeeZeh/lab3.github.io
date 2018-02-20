492c5a1da9bf8b741cb1b74728ac9a3166ade1ee
Jesse Ashmore <Jesse.ashmore97@gmail.com>
2018-02-20 02:25:00 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function calculateAvg(){
+    var table = document.getElementById("table-results");
+    for(var i; i<table.length; i++){
+        var rowTotal = 0;
+        for(var j; j<table.length; j++){
+            if(table.rows[i].cells[j].className === 'table-result' && table.rows[i].cells[j].innerHTML !== '-'){
+                rowTotal +=
+            }
+        }
+    }
+}
