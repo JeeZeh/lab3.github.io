585740e72f379ad6c90f9ab633ab3029bc8b0289
Jesse Ashmore <Jesse.ashmore97@gmail.com>
2018-03-08 21:39:20 +0000

    Add files via upload

--- CS230_Assignment2/main.js
+++ CS230_Assignment2/main.js
@@ -1,6 +1,10 @@
-var assignments = 1;
 var dimensionsHistory = [];
+var assignments;
 var dataHistory = [];
+var highlightedColumn = [];
+var highlighted = "";
+var selected = "red";
+var deSelected = "white";
 
 function addRow(){
     var table = document.getElementById("table-results");
@@ -10,27 +14,28 @@ function addRow(){
     for(var i = 0; i < cols; i++){
         var inserted = row.insertCell(i);
         switch(i){
-            case 0: inserted.innerHTML = "<input class=\"table-input name\" type=\"text\" placeholder=\"-\" onblur=\"saveState();\">"; break;
-            case 1: inserted.innerHTML = "<input class=\"table-input id\" type=\"text\" placeholder=\"-\" onblur=\"saveState();\">"; break;
+            case 0: inserted.innerHTML = "<td><input class=\"table-input name\" type=\"text\" placeholder=\"-\" onclick=\"toggleRow(this.parentElement.parentElement);\"></td>"; break;
+            case 1: inserted.innerHTML = "<input class=\"table-input id\" type=\"text\" placeholder=\"-\" >"; break;
             case (cols-1): inserted.outerHTML = "<td class=\"grade\">-</td>"; break;
-            default: inserted.innerHTML = "<input class=\"table-input assignment\" type=\"number\" placeholder=\"-\" onblur=\"calculateAvg(); saveState();\">";
+            default: inserted.innerHTML = "<input class=\"table-input assignment\" type=\"number\" placeholder=\"-\" onblur=\"calculateAvg();\">";
         }
     }
     calculateAvg();
 }
 
 function generateTable(rows, columns){
+    highlighted = "";
     document.getElementById("table-container").innerHTML = "<table id=\"table-results\">\n" +
         "        <tr id=\"table-header-row\">\n" +
         "            <th id=\"student-header\">Student Name</th>\n" +
         "            <th id=\"id-header\">ID</th>\n" +
-        "            <th class=\"assignment-header\">Assignment 1</th>\n" +
+        "            <th class=\"assignment-header\" onclick=\"toggleColumn(this);\">Assignment 1</th>\n" +
         "            <th id=\"final-header\">Final Grade</th>\n" +
         "        </tr>\n" +
         "        <tr class=\"table-content-row\" id=\"row-template\">\n" +
-        "            <td><input class=\"table-input name\" type=\"text\" placeholder=\"-\" onblur=\"saveState();\"></td>\n" +
-        "            <td><input class=\"table-input id\" type=\"text\" placeholder=\"-\" onblur=\"saveState();\"></td>\n" +
-        "            <td><input class=\"table-input assignment\" type=\"number\" placeholder=\"-\" onblur=\"calculateAvg(); saveState();\"></td>\n" +
+        "            <td><input class=\"table-input name\" type=\"text\" placeholder=\"-\" onclick=\"toggleRow(this.parentElement.parentElement);\"></td>\n" +
+        "            <td><input class=\"table-input id\" type=\"text\" placeholder=\"-\" ></td>\n" +
+        "            <td><input class=\"table-input assignment\" type=\"number\" placeholder=\"-\" onblur=\"calculateAvg();\"></td>\n" +
         "            <td class=\"grade\">-</td>\n" +
         "        </tr>\n" +
         "    </table>";
@@ -50,11 +55,10 @@ function addColumn(){
     for(var i = 0, row; row = table.rows[i]; i++){
         var inserted = row.insertCell(cols-1);
         if(i === 0) {
-            inserted.outerHTML = "<th class=\"assignment-header\">" + "Assignment " + (cols - 2) + "</th>";
-            assignments+=1;
+            inserted.outerHTML = "<th class=\"assignment-header\" onclick=\"toggleColumn(this)\">" + "Assignment " + (cols - 2) + "</th>";
         }
         else{
-            inserted.innerHTML = "<td><input class=\"table-input assignment\" type=\"number\" placeholder=\"-\" onblur=\"calculateAvg(); saveState();\"></td>";
+            inserted.innerHTML = "<td><input class=\"table-input assignment\" type=\"number\" placeholder=\"-\" onblur=\"calculateAvg();\"></td>";
         }
     }
     calculateAvg();
@@ -63,13 +67,14 @@ function addColumn(){
 function calculateAvg() {
     normalise();
     var table = document.getElementById("table-results");
+    assignments = table.rows[0].cells.length - 3;
     for (var i = 1, row; row = table.rows[i]; i++) {
         var rowTotal = 0;
         for (var j = 2, col; col = row.cells[j], j < (table.rows[0].cells.length-1); j++) {
             rowTotal += parseFloat(col.children[0].value) || 0;
         }
         if(assignments > 0) {
-            var mean = Math.round(rowTotal / assignments);
+            var mean = Math.round(rowTotal /assignments);
             row.cells[table.rows[0].cells.length-1].innerText = mean.toString();
         }
     }
@@ -111,6 +116,7 @@ function writeTable(dimensions, data) {
 }
 
 function readTable(){
+    highlighted = "";
     eraseCookie("tableData");
     eraseCookie("tableDimensions");
     var table = document.getElementById("table-results");
@@ -163,8 +169,87 @@ function saveState(){
 
 function undo() {
     writeTable(dimensionsHistory[dimensionsHistory.length-2],dataHistory[dataHistory.length-2]);
+    document.getElementById("btn-undo").disabled = true;
+    highlighted = "";
+    highlightedColumn = [];
 }
 
+function toggleRow(row){
+    if(highlighted === "") {
+        highlighted = row;
+        for(var i = 0; i < row.cells.length; i++){
+            row.cells[i].style.background = selected;
+        }
+        document.getElementById("btn-del-row").disabled = false;
+    }
+    else if(highlighted === row){
+        highlighted = "";
+        for(var i = 0; i < row.cells.length; i++){
+            row.cells[i].style.background = deSelected;
+        }
+
+        document.getElementById("btn-del-row").disabled = false;
+    }
+    else{
+        toggleRow(highlighted);
+        toggleRow(row);
+    }
+}
+
+function toggleColumn(cell){
+    var table = document.getElementById("table-results");
+    var index = parseInt(cell.innerText.split(" ")[1])+1;
+    console.log(index);
+    if(highlightedColumn.length === 0) {
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            for (var j = 0, col; col = row.cells[j]; j++) {
+                if (j === index) {
+                    col.style.background = selected;
+                    highlightedColumn.push(col);
+                    document.getElementById("btn-del-col").disabled = false;
+                }
+            }
+        }
+    }
+    else if(cell.innerText === highlightedColumn[0].innerText){
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            for (var j = 0, col; col = row.cells[j]; j++) {
+                if (j === index) {
+                    col.style.background = deSelected;
+                    highlightedColumn = [];
+                    document.getElementById("btn-del-col").disabled = true;
+                }
+            }
+        }
+    }
+    else{
+        toggleColumn(highlightedColumn[0]);
+        toggleColumn(cell);
+    }
+}
+
+function deleteColumn(){
+    saveState();
+    for(var i = 0; i < highlightedColumn.length; i++){
+        var cell = highlightedColumn[i]; 
+        cell.parentElement.removeChild(cell);
+    }
+    saveState();
+    document.getElementById("btn-undo").disabled = false;
+    document.getElementById("btn-del-col").disabled = true;
+    highlightedColumn = [];
+}
+
+function deleteRow(){
+    saveState();
+    if(highlighted !== null || highlighted !== ""){
+        highlighted.parentElement.removeChild(highlighted);
+    }
+    saveState();
+    document.getElementById("btn-undo").disabled = false;
+    document.getElementById("btn-del-row").disabled = true;
+    highlighted = ""
+}
 
 /*-------------------------*/
 
