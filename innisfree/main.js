f4b27d57c2f1303b982bf44a3a958f269289a047
Jesse Ashmore <Jesse.ashmore97@gmail.com>
2018-04-25 13:35:16 +0100

    publish test

---
+++ innisfree/main.js
@@ -0,0 +1,91 @@
+var lines = 
+[/*2*/"And a small cabin build there, of clay and wattles made,<br>Nine bean-rows will I have there, a hive for the honey-bee.",
+/*4*/"And live alone in the bee-loud glade",
+/*5*/"And I shall have some peace there, for peace comes dropping slow,<br>Dropping from the veils of the morning to where the cricket sings",
+/*7*/"There midnight's all a glimmer, and noon a purple glow,<br>And evening full of the linnet's wings.",
+/*9*/"I will arise and go now, for always night and day<br>I hear lake water lapping with low sounds by the shore"];
+
+var descriptions = 
+["gjhkgjhkgjhksfgdfgfg dfg dg fdg f gdfg df",
+"dfg g dfdfg  gfdfg fdg dgfgfdzdfa ad a g",
+"a gfgd adfgadfgafdgadfg ",
+"afdg fgad dfg  fgdfgdfgd ",
+"a fgdfgdg fdafgd afg fgd fgdfgad"];
+
+var codes = 
+["Home",
+"Innisfree",
+"Isolation",
+"Nature",
+"Solitude"];
+
+var links =
+["https://www.shmoop.com/lake-isle-innisfree/home-theme.html",
+"https://www.shmoop.com/lake-isle-innisfree/innisfree-symbol.html",
+"https://www.shmoop.com/lake-isle-innisfree/isolation-theme.html",
+"https://www.shmoop.com/lake-isle-innisfree/nature-imagery.html",
+"https://www.shmoop.com/lake-isle-innisfree/solitude-imagery.html"
+]
+
+var sound;
+
+function playAudio(file){
+    sound = new Audio("audio\\" + file + ".ogg");
+    sound.play();
+}
+
+function popup(line){
+    line -= 1;
+    document.getElementById('popup').classList.remove("hidden");
+    document.getElementById('dimmer').classList.remove("hidden");
+    playAudio(line);
+    $('#popup-line').html(lines[line]);
+    $('#popup-desc').html(descriptions[line]);
+    $('#popup-qr-link').empty();
+    $('#popup-qr-text').html("Learn more about<br><a href='"+links[line]+"' target='_blank'><b>" + codes[line] + "</b</a>");
+    var img = $('<img>');
+    img.attr("src", "qr/" + line + ".png");
+    img.attr("class", "qr");
+    $('#popup-qr-link').attr("href", links[line]);
+    img.appendTo('#popup-qr-link');
+}
+
+function closePopup(){
+    document.getElementById('popup').classList.add("hidden");
+    document.getElementById('dimmer').classList.add("hidden");
+    sound.pause();
+    sound.currentTime = 0;
+}
+
+$(document).ready(function() {
+    $("#start-button").click(() => {
+        $('#start-button').fadeOut(6000);
+        $('#blocker').fadeOut(6000);
+        playAudio("entry");
+    });
+
+    $('#dimmer').click(() => {
+        closePopup();
+    })
+    
+    $("#hut").click(() =>{
+        popup(1);
+    })
+
+    $("#bees").click(() =>{
+        popup(2);
+    });
+
+    $("#dew").click(() =>{
+        popup(3);
+    });
+
+    $("#linnets").click(() =>{
+        popup(4);
+    });
+
+    $("#sun").click(() =>{
+        popup(5);
+    });
+
+});
