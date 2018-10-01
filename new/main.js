3335e78f2308c2cc9e488818aad118598b220e44
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2018-10-01 17:55:26 +0100

    Added about me section

--- new/main.js
+++ new/main.js
@@ -1,31 +1,29 @@
 typeText = "Web Developer | Programmer | Graphics Designer | Musician";
 typeIterator = 0;
 
-window.onload = function(){
+window.onload = function () {
     this.setTimeout(() => {
         typeIt(this.document.getElementById("heading-sub"), 60);
     }, 150)
 }
 
-function typeIt(element, speed){
-    if(element.innerHTML === "&nbsp;"){
+function typeIt(element, speed) {
+    if (element.innerHTML === "&nbsp;") {
         element.innerHTML = "";
     }
-    if(typeText === ""){
+    if (typeText === "") {
         typeText = element.innerHTML;
         element.innerHTML = "";
     }
-    if(typeIterator < typeText.length){
-        
+    if (typeIterator < typeText.length) {
+
         element.innerHTML += typeText.charAt(typeIterator);
         typeIterator++;
         setTimeout(() => {
             typeIt(element, speed);
         }, speed);
-    }
-    else{
+    } else {
         typeIterator = 0;
         typeText = "";
     }
 }
-
