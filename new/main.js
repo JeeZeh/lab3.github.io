a7481c0707cfdbcb86574d1541c3da7cfe37af1b
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2018-10-02 03:25:39 +0100

    Added resume,animations, portfolio cards and tidied layout

--- new/main.js
+++ new/main.js
@@ -1,5 +1,6 @@
-typeText = "Web Developer | Programmer | Graphics Designer | Musician";
-typeIterator = 0;
+let typeText = "Web Developer | Programmer | Graphics Designer | Musician";
+let typeIterator = 0;
+
 
 window.onload = function () {
     this.setTimeout(() => {
@@ -25,5 +26,27 @@ function typeIt(element, speed) {
     } else {
         typeIterator = 0;
         typeText = "";
+    }
+}
+
+function switchTabs(target){
+    let activeTab = document.querySelector(".active");
+    let hr = document.querySelector("#hr-btm");
+    document.querySelector(".selected").classList.toggle("selected");
+    document.querySelector("#btn-" + target.id.split("-")[1]).classList.toggle("selected");
+    if(activeTab != target){
+        activeTab.classList.toggle("fade");
+        hr.classList.toggle("fade");        
+        setTimeout(() => {
+            activeTab.classList.toggle("active");
+            activeTab.classList.toggle("hide");
+            target.classList.toggle("hide");
+            setTimeout(() => {
+                target.classList.toggle("fade");
+                hr.classList.toggle("fade");
+
+                target.classList.toggle("active");
+            }, 50);
+        }, 400);
     }
 }
